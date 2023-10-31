715ce22399ae91fc923772ffc1d0cb8cc8ba9bd0
Stephen Robinson-Wahl <srobinsonwahl1@babson.edu>
2023-10-31 10:59:34 -0400

    update

--- demo.py
+++ demo.py
@@ -2,16 +2,16 @@
 
 import urllib.request
 import json
-import pprint
+import pprint as p
 
 # api_key = '23469821-d382-4689-9cf5-9b4c0cba76f4'
 url = 'https://services.nvd.nist.gov/rest/json/cves/2.0'
 
 with urllib.request.urlopen(url) as f:
-    response_text = f.read().decode('UTF-8')
+    response_text = f.read().decode()
     # print(response_text)
     j = json.loads(response_text) # j is a dictionary
-    print(j) 
+    # p.pprint(j) 
 
-# for i in j['sources']:
-#     print(i['name'])
+for i in j:
+    print(i.item())
