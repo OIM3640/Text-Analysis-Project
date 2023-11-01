c5ef188098249644acb0577f80be8e22c510c0a4
Immanuel-Ntim-Addae <intimaddae1@babson.edu>
2023-11-01 13:57:16 -0400

    Done.

---
+++ workshop.py
@@ -0,0 +1,14 @@
+import urllib.request
+import string
+import sys
+from unicodedata import category
+
+"""
+This following Project will analyze the text "Phantom of the Opera" by Gaston Leroux. 
+"""
+#Text Analysis Begins
+#Downloaded Version of the book "Phantom of the Opera". 
+f = open('Phantom of the Opera.txt')
+print(f)
+
+print("Hello")
