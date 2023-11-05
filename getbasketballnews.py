55a003692a468fa470ed10c0353de4b23443a5ff
swuuu1 <swu1@babson.edu>
2023-11-05 14:30:48 -0500

    Change private.py to config.py, start getbasketballnews.py

---
+++ getbasketballnews.py
@@ -0,0 +1,21 @@
+from newsapi import NewsApiClient
+from config import NEWSAPI_KEY
+newsapi = NewsApiClient(api_key = NEWSAPI_KEY)
+
+"""
+get basketball news related to certain player, team, or keywords
+"""
+
+
+def getnews():
+    pass
+
+
+
+
+def main():
+    getnews()
+
+
+if __name__ == "__main__":
+    main()
