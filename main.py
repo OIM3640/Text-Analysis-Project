41216744649db2a00b747be5762b514358db3abb
jamitai <jamitai1@babson.edu>
2024-10-31 18:20:58 -0400

    Text comparison

--- main.py
+++ main.py
@@ -1,76 +1,75 @@
-
 from collections import Counter
-import re 
-import urllib.request
-import pickle
-import os
-
-def download_book(gutenberg_url,file_name):
-    """
-    
-    """
-    #if we have already downloaded book to disk, just read book from disk to avoid api request
-    if os.path.exists(file_name):
-        with open(file_name,'rb') as f:
-            reloaded_copy_of_texts = pickle.load(f)
-            return reloaded_copy_of_texts
-    
-    #if we don't have copy of book already, save to disk for future references
-    else:
-        with urllib.request.urlopen(gutenberg_url) as f:
-            text = f.read().decode('utf-8')
-        with open(file_name,'wb') as f:
-            pickle.dump(text,f)
-            return text
+from nltk.corpus import stopwords  
+from thefuzz import fuzz
+from utilities import download_book, clean_text
+from visualize_similarities import compare_fitz_shakespeare
 
 def analyze_text(text):
-    words=text.split()
-    counts=dict()
+    """
+    Analyzes word frequency in the given text, returning a dictionary with words as keys and 
+    their counts as values.
+    
+    Args:
+        text (str): The text to analyze.
+    
+    Returns:
+        dict: A dictionary where keys are words and values are the frequency of each word.
+    """
+    words = text.split()
+    counts = dict()
     for word in words:
         if word in counts:
-            counts[word]=counts[word]+1
+            counts[word] += 1
         else:
-            counts[word]=1
+            counts[word] = 1
     return counts
 
 def top_words(text):
-    counts=Counter(text.split())
+    """
+    Prints the top 10 most common words in the given text.
+    
+    Args:
+        text (str): The text to analyze for top words.
+    """
+    counts = Counter(text.split())
     print(counts.most_common(10))
 
-STOP_WORDS = {"so","he","she","but", "my","his","him","my","me","you","at","her","had", "we", "i", "and", "the", "a", "an", "in", "to", "of", "that", "it", "is", "for", "with", "as", "on", "this", "by", "was", "be", "or", "are"}
-def clean_text(text):
-    """
-    TO DO: Set up function to clean text
-    periods, question marks, quotes, :, 
-    Remove stop words (So, we, I, and, the)
-    """
-    #Make sure all words are lowercase
-    text=text.lower() 
-    #Make sure punctuation is removed from words in text
-    text = re.sub(r'[^\w\s]', '', text)
-    
-    # Remove stop words
-    words = text.split()
-    filtered_words = [word for word in words if word not in STOP_WORDS]
-    
-    # Join filtered words back into a single string
-    cleaned_text = " ".join(filtered_words)
-    
-    return cleaned_text
-    #Make every word lowercase
-    ##text=text.lower() 
-    ##return text
 
 
-def main():
-    gatsby_url = 'https://www.gutenberg.org/cache/epub/64317/pg64317.txt'
-    file_name="gatsby_text.pkl"
-    text=download_book(gatsby_url,file_name)
-    cleaned_text=clean_text(text)
-    print(analyze_text(cleaned_text))
+def analyze_book_text(cleaned_text):
+    """
+    Analyzes and prints word frequencies and top words in the cleaned text.
+    
+    Args:
+        cleaned_text (str): The cleaned text to analyze.
+    """
     top_words(cleaned_text)
 
+def main():
+    """
+    Main function to download, clean, and analyze texts from two books and calculate text similarity.
+    Downloads 'The Great Gatsby' and 'This Side of Paradise' from Project Gutenberg, cleans the text, 
+    analyzes top words, and calculates the similarity ratio between the two books.
+    """
+    gatsby_url = 'https://www.gutenberg.org/cache/epub/64317/pg64317.txt'
+    gatsby_file_name = "gatsby_text.pkl"
+    gatsby_text = download_book(gatsby_url, gatsby_file_name)
+    gatsby_cleaned_text = clean_text(gatsby_text)
+    analyze_book_text(gatsby_cleaned_text)
+    
+    paradise_url = 'https://www.gutenberg.org/cache/epub/805/pg805.txt'
+    paradise_file_name = "paradise_text.pkl"
+    paradise_text = download_book(paradise_url, paradise_file_name)
+    paradise_cleaned_text = clean_text(paradise_text)
+    analyze_book_text(paradise_cleaned_text)
 
-#run program by running main
+    # Text Similarity
+    print(fuzz.ratio(gatsby_cleaned_text, paradise_cleaned_text))  # Similarity ratio
+
+    #Compare Fitzgerald and Shakespeare
+    compare_fitz_shakespeare()
+
+    
+# Run program by running main
 if __name__ == "__main__":
     main()
