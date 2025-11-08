b4c8f038bc97769e04ab2fcffa67ee52706fc1ff
starduxs <klin4@babson.edu>
2025-11-08 00:59:13 -0500

    Complete
    Completed Assignment, sorry its late had trouble with submitting it

---
+++ # text_analysis_project.py
@@ -0,0 +1,6 @@
+# text_analysis_project
+├── fetch_text.py         # Fetches Wikipedia article
+├── clean_text.py         # Cleans and normalizes text
+├── analyze_text.py       # Filters stopwords, counts word frequency, plots results
+├── main.py               # Entry point to run the full pipeline
+├── README.md             # Project overview and documentation
